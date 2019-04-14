99251a5bab1cd0142315f9aaaffa62f56b5a2e23
Karthik2630 <49606935+Karthik2630@users.noreply.github.com>
2019-04-14 14:24:04 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        *{
+            padding:0px;
+            margin:0px;
+        }
+        body
+        {
+            background:yellow;
+        }
+        #cont-main
+        {
+            width:50%;
+            background:grey;
+            padding:5px;
+            margin:10px auto;
+        }
+        #top-cont
+        {
+            width:100%;
+            //background:blue;
+            padding-top:30px;
+        }
+        #mid-cont
+        {
+            width:100%;
+            background:green;
+            padding-top:250px;
+        }
+
+    </style>
+</head>
+<body>
+<div id="cont-main">
+
+    <div id="top-cont">
+
+    </div>
+
+    <div id="mid-cont">
+
+    </div>
+
+    <div id="btm-cont">
+
+    </div>
+
+</div>
+<?php
+
+
+?>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CB-2630</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/style.css">
+    <!--<link rel="icon" href="../images/logo.png" type="image/png" sizes="20x20" style="border-radius:50px;">-->
+</head>
+<body>
+<div id="cont-main">
+
+    <div id="top-cont">
+        <label>The Glory Of <i>Friendship</i></label>
+    </div>
+
+    <div id="mid-cont">
+        <div id="messages">
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+        
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+        
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+        
+        <div class="question">
+            <p class="">Your Name</p>
+        </div>
+        <div class="response">
+            <p class="">Neeru</p>
+        </div>
+
+    </div>
+    </div>
+
+    <div id="btm-cont">
+
+    </div>
+
+</div>
+<?php
+
+
+?>
